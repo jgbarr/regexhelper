f2b68dcbe777a27709d974add7cfae07b7b2dc0e
Jeff Barr <jeff@jeffbarr.com>
2011-05-18 18:08:06 -0700

    add comments

--- lib/regexhelper.rb
+++ lib/regexhelper.rb
@@ -1,11 +1,12 @@
 module REGEXHELPER
   def self.email_regex
+    #email addresses with <= 3 letter .tld
     email_regex ||= Regexp.new('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
     return email_regex
   end
 
   def self.image_regex
-    #only images with chars and/or numbers in the filename
+    #only images with characters and/or numbers in the filename, and an extension of .png, .jpg, .gif - ignores case.
     image_regex ||= Regexp.new('^.*([^\.][\.](([gG][iI][fF])|([Jj][pP][Gg])|([Jj][pP][Ee][Gg])|([Bb][mM][pP])|([Pp][nN][Gg])))')
     return image_regex
   end
